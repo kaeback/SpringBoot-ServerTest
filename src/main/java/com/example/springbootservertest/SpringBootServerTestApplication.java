e04b6669b237857b3d20489686b2a33a1d4b8052
kaeback <kaeback@gmail.com>
2023-06-25 14:53:30 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootservertest/SpringBootServerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootservertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootServerTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootservertest/SpringBootServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootservertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
